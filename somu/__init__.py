3049cf405e6d79faa898d2c1913ab301a5a1d4c5
ventus <ventuss550@gmail.com>
2025-08-18 00:04:28 +0200

    integrate and patch torchsom

--- somu/__init__.py
+++ somu/__init__.py
@@ -1,1 +1,8 @@
-from .somu import *
+try:
+    from .somu import som
+except ImportError:
+    from .torchsom import som
+
+    print("Failed to import somu. Falling back to the torch implementation.")
+
+__all__ = ["som"]
